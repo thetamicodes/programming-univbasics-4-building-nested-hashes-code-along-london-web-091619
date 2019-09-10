5ac2093457d5168b88aa66ea6fec368151b44d37
Tami Reichold <reichold.tamara@gmail.com>
2019-09-10 16:33:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -24,8 +24,6 @@ def third_challenge
         name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44", attitude: "worried"}
         ]
    }
   }
