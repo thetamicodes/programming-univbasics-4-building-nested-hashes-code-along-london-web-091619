c8da181e1c1e7748c7e71a7821974580559e4a1a
Tami Reichold <reichold.tamara@gmail.com>
2019-09-10 16:32:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,8 +11,6 @@ def third_challenge
         name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
    }, 
    :capulet => {
